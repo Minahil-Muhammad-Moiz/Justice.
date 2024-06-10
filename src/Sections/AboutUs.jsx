9a2978cb65842f596a7dd07e7476c6adf939fccd
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-06-11 00:23:11 +0500

    completed AboutUs section

--- src/Sections/AboutUs.jsx
+++ src/Sections/AboutUs.jsx
@@ -1,9 +1,7 @@
 import classNames from "classnames";
 import React from "react";
-import { BiBriefcase, BiTargetLock } from "react-icons/bi";
-import { BsPeople } from "react-icons/bs";
 import { FaArrowRight } from "react-icons/fa6";
-import { MdCorporateFare } from "react-icons/md";
+import { practiceAreas } from "../Components/Contants";
 
 const AboutUs = () => {
   return (
@@ -14,47 +12,33 @@ const AboutUs = () => {
         </h2>
       </div>
       <div className={classNames(divClass)}>
-        <div className={classNames(subDivs)}>
-          <div className={classNames(icons)}>
-            <BiBriefcase />
+        {practiceAreas.map((item) => (
+          <div className={classNames(subDivs)}>
+            <div className={classNames(icons)}>{item.icon}</div>
+            <div className={classNames(pTag)}>
+              <p className={classNames(iconPTag)}>{item.title}</p>
+              <p>{item.desp}</p>
+              <div className={classNames(btnClass)}>
+                <p
+                  className={classNames(
+                    transitionDelay,
+                    "group-hover:text-amber-600"
+                  )}
+                >
+                  Learn More
+                </p>
+                <i
+                  className={classNames(
+                    transitionDelay,
+                    "group-hover:bg-amber-600 group-hover:text-white p-2"
+                  )}
+                >
+                  <FaArrowRight />
+                </i>
+              </div>
+            </div>
           </div>
-          <div className={classNames(pTag, "px-4")}>
-            <p className={classNames(iconPTag)}>Labor and
-            Employment</p>
-            <p>Far far away, far from the mountains, far from the countries</p>
-          </div>
-        </div>
-        <div className={classNames(subDivs)}>
-          <div className={classNames(icons)}>
-            <BsPeople />
-          </div>
-          <div className={classNames(pTag, "px-4")}>
-            <p className={classNames(iconPTag)}>Intellectual
-            Proerty Law</p>
-            <p>Far far away, far from the mountains, far from the countries</p>
-            <div classNames={classNames('flex hover:text-amber-600 cursor-pointer')}><p>Learn More</p><i><FaArrowRight/></i></div>
-          </div>
-        </div>
-        <div className={classNames(subDivs)}>
-          <div className={classNames(icons)}>
-            <MdCorporateFare />
-          </div>
-          <div className={classNames(pTag, "px-4")}>
-            <p className={classNames(iconPTag)}>Corporate & Civil
-            Litigation</p>
-            <p>Far far away, far from the mountains, far from the countries</p>
-          </div>
-        </div>
-        <div className={classNames(subDivs)}>
-          <div className={classNames(icons)}>
-            <BiTargetLock />
-          </div>
-          <div className={classNames(pTag, "px-4")}>
-            <p className={classNames(iconPTag)}>Criminal Prosecution
-            and Defense</p>
-            <p>Far far away, far from the mountains, far from the countries</p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
@@ -68,7 +52,10 @@ const h2Tag = "text-white text-4xl md:text-6xl ";
 const divClass =
   " m-4 flex flex-col md:flex-row justify-between items-stretch md:flex-wrap w-[90%] md:w-[80%] p-4 ";
 const subDivs =
-  "flex flex-col justify-center items-start p-4 my-2 md:m-2 w-full md:w-[45%] lg:w-[22%] bg-white rounded-3xl";
-const pTag = "tracking-wide";
+  "flex flex-col justify-center items-start px-2 py-4 my-2 md:m-2 w-full md:w-[45%] lg:w-[22%] bg-white rounded-xl";
+const pTag = "tracking-wide px-2 py-6";
 const icons = "p-4 mx-2 rounded-full bg-amber-600 text-white text-2xl";
-const iconPTag = 'text-amber-600 font-medium';
+const iconPTag = "text-amber-600 font-medium";
+const transitionDelay = "transition-all delay-150";
+const btnClass =
+  "flex justify-between items-center w-28 text-black text-sm my-2 cursor-pointer group";
