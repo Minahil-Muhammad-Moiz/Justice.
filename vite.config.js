14ea40c40ad3c4bc5c530efe647ec0c869e58831
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-06-19 17:25:07 +0500

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  basse: '/typescript-with-react/',
+  basse: '/lawConnect/',
   plugins: [react()],
 })
